7865b7d9e2e3bdcfad02f208d643aed77605fa9e
Aleemr302 <148830790+Aleemr302@users.noreply.github.com>
2025-07-24 16:07:34 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
 
-const nextConfig: NextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
-  typescript: {
-    // ignoreBuildErrors: true,
-  },
-};
-
-export default nextConfig;
+module.exports = nextConfig
